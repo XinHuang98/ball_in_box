cc8fb89fd0740ea48d5a8accfea3cc9b6855a390
XinHuang98 <763479239@qq.com>
2018-06-01 12:44:59 +0800

    add test_cases and algorithm

--- ball_in_box/area_sum.py
+++ ball_in_box/area_sum.py
@@ -1,6 +1,12 @@
 import math
+import time
+import ball_in_box
 import ball_in_box.ballinbox as bb
 import ball_in_box.validate as val
+
+# DragoonKiller
+# According to the test_docker_image.sh,
+# this file will be replaced by another file when testing.
 
 def area_sum(circles):
     area = 0.0
@@ -10,18 +16,18 @@ def area_sum(circles):
     return area
 
 if __name__ == '__main__':
+    
+    rec = time.clock()
+    
     num_of_circle = 5
-    blockers = [(0.5, 0.5)
-               ,(0.5, -0.3)]
+    blockers = [(0.8, 0.8), (0.8, -0.8), (-0.8, 0.8), (-0.8, -0.8)]
     
     circles = bb.ball_in_box(num_of_circle, blockers)
-    
+
     if num_of_circle == len(circles) and val.validate(circles, blockers):
         area = area_sum(circles)
         print("Total area: {}".format(area))
     else:
         print("Error: no good circles.")
-
-
-
-
+    
+    print("time usage: {}s".format(time.clock() - rec))
--- ball_in_box/ball_in_box/validate.py
+++ ball_in_box/ball_in_box/validate.py
@@ -23,20 +23,21 @@ def validate(circles, blockers):
                 r = circle[2]
                 bx = block[0]
                 by = block[1]
-                if math.sqrt((x - bx)**2 + (y - by)**2) < r:
+                if (x - bx)**2 + (y - by)**2 < r**2:
                     return False
 
     # Is circle good for each other?
     for circle1 in circles:
         for circle2 in circles:
             x1 = circle1[0]
-            y1 = circle1[0]
-            r1 = circle1[0]
+            y1 = circle1[1]
+            r1 = circle1[2]
             x2 = circle2[0]
-            y2 = circle2[0]
-            r2 = circle2[0]
-            if math.sqrt((x1 - x2)**2 + (y1 - y2)**2) < (r1 + r2):
-                return False
-
+            y2 = circle2[1]
+            r2 = circle2[2]
+            if x1 != x2 and y1 != y2:
+                if (x1 - x2)**2 + (y1 - y2)**2 < (r1 + r2)**2:
+                    return False
+    
     # all good
     return True
